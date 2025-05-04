315587261e7d71ecfffcb334e91cd5b7d7f16cc2
Your Name <you@example.com>
2025-05-04 15:54:35 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -47,7 +47,38 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
+    if(tree == NULL) return; // Verifica si el árbol es NULL
 
+    if(tree->root == NULL){ // Si el árbol está vacío, crea el nodo raíz
+        tree->root = createTreeNode(key, value); // Crea un nuevo nodo con la clave y el valor proporcionados
+        tree->current = tree->root; // Establece el nodo actual como la raíz
+        return;
+    }
+
+    TreeNode* current = tree->root; // Inicializa el nodo actual como la raíz
+    TreeNode* parent = NULL; // Inicializa el nodo padre como NULL
+
+    while (current != NULL){ // Recorre el árbol hasta encontrar la posición adecuada para insertar el nuevo nodo
+        parent = current; // Guarda el nodo padre actual
+        if(tree->lower_than(key,current->pair->key)){ // Si la clave es menor que la clave del nodo actual
+            current = current->left; // Mueve hacia la izquierda
+        } else if(tree->lower_than(current->pair->key, key)){ // Si la clave es mayor que la clave del nodo actual
+            current = current->right; // Mueve hacia la derecha
+        } else{ // Si la clave es igual a la clave del nodo actual
+            current->pair->value = value; // Actualiza el valor del nodo actual
+            return; // Sale de la función
+        }
+    }
+
+    TreeNode* newNode = createTreeNode(key, value); // Crea un nuevo nodo con la clave y el valor proporcionados
+    if (newNode == NULL) return; // Verifica si la creación del nuevo nodo fue exitosa
+
+    newNode->parent = parent; // Establece el padre del nuevo nodo
+    if(tree->lower_than(key,parent->pair->key)){ // Si la clave es menor que la clave del nodo padre
+        parent->left = newNode; // Inserta el nuevo nodo como hijo izquierdo del nodo padre
+    } else{ // Si la clave es mayor o igual que la clave del nodo padre
+        parent->right = newNode; // Inserta el nuevo nodo como hijo derecho del nodo padre
+    }
 }
 
 TreeNode * minimum(TreeNode * x){
